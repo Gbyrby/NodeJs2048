25a87b577c9047b82142413e2c6ae7657901b35d
Gbyrby <ANDREYPINKINYX@yandex.ru>
2026-02-04 01:15:28 +0300

    add index.html with embedded styles and server HTML serving

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 const http = require("http");
 const url = require('url');
 const { parse } = require('querystring');
-
-http.createServer(function(request,response){
-    const path = url.parse(request.url,true).pathname;
-
-    response.end(path);
+const fs = require("fs");
+const path = require("path");
 
 
-    
-}).listen(3000, "127.0.0.1",function(){
+http.createServer(function (request, response) {
+    const pathparam = url.parse(request.url, true).pathname;
+
+    if (request.method == "GET" && pathparam == "/") {
+        const filePath = path.join(__dirname, 'index.html');
+
+      fs.readFile(filePath, (err, data) => {
+        if (err) {
+          response.statusCode = 500;
+          response.end("Server error");
+          return;
+        }
+        response.setHeader('Content-Type','text/html'+ '; charset=utf-8');
+        response.end(data);
+      });
+      return;
+    }else{
+        response.end(request.method+' '+pathparam);
+    }
+  })
+  .listen(3000, "127.0.0.1", function () {
     console.log("Сервер начал прослушивание запросов на порту 3000");
-});
+  });
