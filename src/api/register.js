de10010b960c3fb9b08fcb57567a867cd3922ea2
Gbyrby <ANDREYPINKINYX@yandex.ru>
2026-02-05 12:52:21 +0300

    Added getRequestBody for username

--- src/api/register.js
+++ src/api/register.js
@@ -17,12 +17,32 @@ function parseCookies(req) {
     );
 }
 
+async function getRequestBody(req) {
+    return new Promise((resolve, reject) => {
+        let body = "";
+        req.on("data", (chunk) => {
+            body += chunk.toString();
+        });
+        req.on("end", () => {
+            try {
+                resolve(JSON.parse(body));
+            } catch (e) {
+                reject(e);
+            }
+        });
+        req.on("error", (err) => reject(err));
+    });
+}
+
 module.exports = [
     {
         method: "POST",
         path: "register",
-        handler(request, response) {
-            const user = createUser("TestName");
+        handler: async function (request, response) {
+            const data = await getRequestBody(request);
+            const username = data.username;
+
+            const user = createUser(username);
 
             const cookie = [`SessionID=${user.SessionID}; Max-Age=10;Path=/;`];
             response.setHeader("Set-Cookie", cookie);
