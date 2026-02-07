ee92b7ba7bfd75ed64fadfb3331fa0567b95742e
Gbyrby <ANDREYPINKINYX@yandex.ru>
2026-02-07 11:41:23 +0300

    performance.now() for response time

--- src/app.js
+++ src/app.js
@@ -3,7 +3,10 @@ const http = require("http");
 const router = require("./router");
 const port = process.env.PORT || 3000;
 http.createServer(function (request, response) {
+    const start = performance.now();
     router(request, response);
+    const end = performance.now();
+    console.log(`Время выполнения: ${end - start} мс`);
 }).listen(port, "0.0.0.0", function () {
     console.log(`Сервер начал прослушивание запросов на порту ${port}`);
 });
